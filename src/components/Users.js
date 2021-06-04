1926eb0895fb0dbba7d38ed17728bcf4bbe10933
Nitesh Bagle <niteshbagle43@gmail.com>
2021-06-05 00:28:50 +0530

    wip

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,20 +3,6 @@ import User from './User';
 
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
-
-const usersList = (
-  <ul>
-    {DUMMY_USERS.map((user) => (
-      <User key={user.id} name={user.name} />
-    ))}
-  </ul>
-);
-
 class Users extends Component {
   constructor() {
     super();
@@ -32,6 +18,14 @@ class Users extends Component {
   };
 
   render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
@@ -42,52 +36,5 @@ class Users extends Component {
     );
   }
 }
-// const Users = () => {
-//   const [showUsers, setShowUsers] = useState(true);
-
-//   const toggleUsersHandler = () => {
-//     setShowUsers((curState) => !curState);
-//   };
-
-//   return (
-//     <div className={classes.users}>
-//       <button onClick={toggleUsersHandler}>
-//         {showUsers ? 'Hide' : 'Show'} Users
-//       </button>
-//       {showUsers && usersList}
-//     </div>
-//   );
-// };
-
-// const DUMMY_USERS = [
-//   { id: 'u1', name: 'Max' },
-//   { id: 'u2', name: 'Manuel' },
-//   { id: 'u3', name: 'Julie' },
-// ];
-
-// const Users = () => {
-//   const [showUsers, setShowUsers] = useState(true);
-
-//   const toggleUsersHandler = () => {
-//     setShowUsers((curState) => !curState);
-//   };
-
-//   const usersList = (
-//     <ul>
-//       {DUMMY_USERS.map((user) => (
-//         <User key={user.id} name={user.name} />
-//       ))}
-//     </ul>
-//   );
-
-//   return (
-//     <div className={classes.users}>
-//       <button onClick={toggleUsersHandler}>
-//         {showUsers ? 'Hide' : 'Show'} Users
-//       </button>
-//       {showUsers && usersList}
-//     </div>
-//   );
-// };
 
 export default Users;
